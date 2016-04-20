ab06fd87675b694f692575aade15416a5d49e478
Choumiko <ghostface@ghostface.info>
2016-04-20 18:37:25 +0200

    add stdlib

---
+++ stdlib/entity/entity.lua
@@ -0,0 +1,57 @@
+--- Entity module
+-- @module entity
+
+require 'stdlib/core'
+require 'stdlib/surface'
+require 'stdlib/area/area'
+
+Entity = {}
+
+--- Converts an entity and its selection_box to the area around it
+-- @param entity to convert to an area
+-- @return area that entity selection_box is valid for
+function Entity.to_selection_area(entity)
+    fail_if_missing(entity, "missing entity argument")
+
+    local pos = entity.position
+    local bb = entity.prototype.selection_box
+    return Area.offset(bb, pos)
+end
+
+--- Converts an entity and its selection_box to the area around it
+-- @param entity to convert to an area
+-- @return area that entity selection_box is valid for
+function Entity.to_collision_area(entity)
+    fail_if_missing(entity, "missing entity argument")
+
+    local pos = entity.position
+    local bb = entity.prototype.collision_box
+    return Area.offset(bb, pos)
+end
+
+--- Returns the backer name of the entity
+-- @param entity to get the backer name from
+-- @return backer name or false if the entity can't have a backer name
+function Entity.get_backer_name(entity)
+    fail_if_missing(entity, "missing entity argument")
+
+    local s, backer_name = pcall(function() return entity.backer_name end)
+    return s and backer_name
+end
+
+--- Sets the backer name of the entity
+-- @param entity to set the backer name
+-- @param name new backer name for the entity
+-- @return true if renaming was successful
+function Entity.set_backer_name(entity, name)
+    fail_if_missing(entity, "missing entity argument")
+    fail_if_missing(name, "missing name argument")
+
+    if Entity.get_backer_name(entity) then
+        entity.backer_name = name
+        return true
+    end
+    return false
+end
+
+return Entity
